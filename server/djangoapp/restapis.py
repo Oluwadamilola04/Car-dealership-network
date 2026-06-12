8dd53e55f5be327b0a0d7fa7e42c82fb823bb92c
Oluwadamilola Akinrinmade <119226082+Oluwadamilola04@users.noreply.github.com>
2026-06-12 20:07:59 +0100

    Add analyze_review_sentiments function
    Implemented the analyze_review_sentiments function to call the sentiment analysis API and handle exceptions.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,8 +14,15 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
